69814a7146710b329b1bb80ba7de67e3e1d0e777
Jens Bassem Hanna Issa <jens7391@easv365.dk>
2022-11-25 11:04:45 +0100

    tests

--- API/Program.cs
+++ API/Program.cs
@@ -37,6 +37,7 @@ builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSet
 builder.Services.AddDbContext<RepositoryDBContext>(options => options.UseSqlite("Data Source =db.db"));
 builder.Services.AddScoped<RepositoryDBContext>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IRebuildService, RebuildService>();
 builder.Services.AddScoped<IRebuildRepository, RebuildRepository>();
