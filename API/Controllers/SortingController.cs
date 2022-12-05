c53c36f75b829335785ab49178918b2b05725f01
JensIssa <71692396+JensIssa@users.noreply.github.com>
2022-12-05 13:45:14 +0100

    send this to test

--- API/Controllers/SortingController.cs
+++ API/Controllers/SortingController.cs
@@ -14,9 +14,16 @@ public class SortingController
     }
 
     [HttpGet]
-    [Route("GetAllProductsFromPrice")]
+    [Route("GetAllProductsFromPriceLowToHigh")]
     public List<Product> GetAllProductsFromPrice()
     {
         return _service.SortProductsByPrice();
+    }
+
+    [HttpGet]
+    [Route("GetAllProductsHighToLow")]
+    public List<Product> GetAllProductsFromPriceReverse()
+    {
+        return _service.SortProductsByPriceReverse();
     }
 }
