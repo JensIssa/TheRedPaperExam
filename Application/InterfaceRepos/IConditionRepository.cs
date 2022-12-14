dff7c9ddccaeb168c3007961c9a10ad0714f169d
MathiasKrarup <math249e@easv365.dk>
2022-12-14 23:12:12 +0100

    Kommentare til InterfaceRepo

--- Application/InterfaceRepos/IConditionRepository.cs
+++ Application/InterfaceRepos/IConditionRepository.cs
@@ -4,5 +4,9 @@ namespace Application.InterfaceRepos;
 
 public interface IConditionRepository
 {
+    /// <summary>
+    /// This method returns a list of all the conditions from the database
+    /// </summary>
+    /// <returns>A list of all the condition objects in the database</returns>
     List<Condition> GetConditions();
 }
--- Application/InterfaceRepos/IOrderRepository.cs
+++ Application/InterfaceRepos/IOrderRepository.cs
@@ -4,9 +4,23 @@ namespace Application.InterfaceRepos;
 
 public interface IOrderRepository
 {
+    /// <summary>
+    /// This method creates an order object to the database
+    /// </summary>
+    /// <param name="order">The order object</param>
+    /// <returns>A new order object</returns>
     Order CreateOrder(Order order);
 
+    /// <summary>
+    /// This method returns a list of all the order objects from the database
+    /// </summary>
+    /// <returns>A list of all the order objects</returns>
     List<Order> GetAllOrders();
 
+    /// <summary>
+    /// This method returns a list of all the order objects connected to a specific user from the database 
+    /// </summary>
+    /// <param name="id">The id of the specific user object the order objects are connected to</param>
+    /// <returns>a list of all the order objects connected to a specific user</returns>
     List<Order> GetAllOrdersByUser(int id);
 }
